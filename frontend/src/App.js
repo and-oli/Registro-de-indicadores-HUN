56df65af7b1764a017923bbfaab1d7cba5eb1501
Andrés <af.olivares10@uniandes.edu.co>
2020-04-18 22:16:45 -0500

    Incluir autenticación

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-//import SignIn from './Shared/SignIn'
+import SignIn from './Shared/SignIn'
 import Navigation from './Shared/Navigation'
 
 function App() {
   return (
     <div className="App">
-      {/*<SignIn />*/}
-      <Navigation/>
+      {
+        (localStorage.getItem("HUNToken")) ?
+          <Navigation />:
+          <SignIn /> 
+      }
     </div>
   );
 }
--- frontend/src/Shared/SignIn.js
+++ frontend/src/Shared/SignIn.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -46,72 +27,92 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
 export default function SignIn() {
   const classes = useStyles();
-  return ( 
-      <Container component="main" maxWidth="xs">
+  const [username, setUsername] = React.useState("")
+  const [password, setPassword] = React.useState("")
+  const [message, setMessage] = React.useState({ text: "", color: "green" })
+  const [loading, setLoading] = React.useState(false)
+
+  const submitCredentials = function (e) {
+    e.preventDefault();
+    if (!loading) {
+      setLoading(true)
+      let data = { username, password }
+      fetch("/users/authenticate", {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+      }).then((response) => response.json())
+        .then((responseJson) => {
+          setLoading(false)
+          if (responseJson.success) {
+            localStorage.setItem("HUNToken", responseJson.token);
+            window.location.reload();
+            setMessage({ color: "green", text: responseJson.message })
+          } else {
+            setMessage({ color: "red", text: responseJson.message })
+          }
+        })
+    }
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
         <Typography component="h1" variant="h5">
           Iniciar Sesión
         </Typography>
-        <form className={classes.form} noValidate>
+        <form
+          className={classes.form}
+          onSubmit={submitCredentials}
+        >
           <TextField
+            onChange={(e) => { setUsername(e.target.value) }}
             variant="outlined"
             margin="normal"
             required
             fullWidth
             id="email"
+            type="email"
             label="Correo Electrónico"
             name="email"
+            value={username}
             autoComplete="email"
             autoFocus
           />
           <TextField
+            onChange={(e) => { setPassword(e.target.value) }}
             variant="outlined"
             margin="normal"
             required
             fullWidth
+            value={password}
             name="password"
             label="Contraseña"
             type="password"
             id="password"
             autoComplete="current-password"
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Rocrdarme"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Iniciar Sesión
+          {loading ?
+            <div class="loader"></div> :
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Iniciar Sesión
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                ¿Olvidó su contraseña?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
+          }
         </form>
+        <div style={{ color: message.color }}>{message.text}</div>
       </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>         
+    </Container>
   );
 }
